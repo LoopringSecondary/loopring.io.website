3b433b639b37522bb0e18b08c65de06c1fd0bb61
wangdong <wangdong@zhongan.io>
2020-11-25 21:33:01 +0800

    Updates

---
+++ precache-manifest.4c34c22a81c540f75482438c5b0e2551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a1c53bd2331a024c1122baab50d552",
+    "url": "//loopring.io/index.html"
+  },
+  {
+    "revision": "5265c5d0bdc0c951b501",
+    "url": "//loopring.io/static/css/2.b7dc273e.chunk.css"
+  },
+  {
+    "revision": "1e043903772c5ac9eb6a",
+    "url": "//loopring.io/static/css/main.1edb6351.chunk.css"
+  },
+  {
+    "revision": "5265c5d0bdc0c951b501",
+    "url": "//loopring.io/static/js/2.a654cb09.chunk.js"
+  },
+  {
+    "revision": "4b81d1e7bd123e6b656794ec83c34190",
+    "url": "//loopring.io/static/js/2.a654cb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e043903772c5ac9eb6a",
+    "url": "//loopring.io/static/js/main.1498f852.chunk.js"
+  },
+  {
+    "revision": "f7083f60f8d2dc24a2a2",
+    "url": "//loopring.io/static/js/runtime-main.9d179c5f.js"
+  }
+]);
