f6ae3fb27ca15cf99d6aef81d25492ecb2490206
wangdong <wangdong@zhongan.io>
2020-12-08 21:33:49 +0800

    Updates

--- precache-manifest.73fa11b85f01a4cc4a896c03f9626c28.js
+++ precache-manifest.73fa11b85f01a4cc4a896c03f9626c28.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b6fe11eecdd3a5c7dfd19b67d1183e65",
+    "revision": "499ffac42c70293a28881bf7e397bce3",
     "url": "//loopring.io/index.html"
   },
   {
-    "revision": "92641d70526af6ba2d1b",
+    "revision": "3e2b63f2c095188ae15f",
     "url": "//loopring.io/static/css/2.b7dc273e.chunk.css"
   },
   {
-    "revision": "4cd93502caf1cb157e1e",
+    "revision": "c38d877215ae61c87db5",
     "url": "//loopring.io/static/css/main.fcf760fa.chunk.css"
   },
   {
-    "revision": "92641d70526af6ba2d1b",
-    "url": "//loopring.io/static/js/2.d8a29fc8.chunk.js"
+    "revision": "3e2b63f2c095188ae15f",
+    "url": "//loopring.io/static/js/2.da22ad7c.chunk.js"
   },
   {
     "revision": "4b81d1e7bd123e6b656794ec83c34190",
-    "url": "//loopring.io/static/js/2.d8a29fc8.chunk.js.LICENSE.txt"
+    "url": "//loopring.io/static/js/2.da22ad7c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4cd93502caf1cb157e1e",
-    "url": "//loopring.io/static/js/main.ae04c5e7.chunk.js"
+    "revision": "c38d877215ae61c87db5",
+    "url": "//loopring.io/static/js/main.d32db9df.chunk.js"
   },
   {
     "revision": "f7083f60f8d2dc24a2a2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//loopring.io/precache-manifest.8157d44d65d61968dd781d1380ed9b39.js"
+  "//loopring.io/precache-manifest.73fa11b85f01a4cc4a896c03f9626c28.js"
 );
 
 self.addEventListener('message', (event) => {
