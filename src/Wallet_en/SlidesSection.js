4216af5cbe834db40c381505dfbd976c1689016b
Daniel Wang <daniel@loopring.org>
2020-12-30 15:49:20 +0800

    updated promotion images (#19)

--- src/Wallet_en/SlidesSection.js
+++ src/Wallet_en/SlidesSection.js
@@ -23,21 +23,21 @@ const SlidesSection = () => {
       target="_blank"
       className="slide"
     >
-      <img src="/images/promotions/1_en.jpg" onDragStart={handleDragStart} />
+      <img src="/images/promotions/1_en.png" onDragStart={handleDragStart} />
     </a>,
     <a
       href="https://medium.com/loopring-protocol/loopring-to-launch-amm-liquidity-mining-on-layer-2-6cabbcf4801c?source=collection_home---4------0-----------------------"
       target="_blank"
       className="slide"
     >
-      <img src="/images/promotions/2_en.jpg" onDragStart={handleDragStart} />
+      <img src="/images/promotions/2_en.png" onDragStart={handleDragStart} />
     </a>,
     <a
       href="https://medium.com/loopring-protocol/loopring-to-launch-amm-liquidity-mining-on-layer-2-6cabbcf4801c?source=collection_home---4------0-----------------------"
       target="_blank"
       className="slide"
     >
-      <img src="/images/promotions/3_en.jpg" onDragStart={handleDragStart} />
+      <img src="/images/promotions/3_en.png" onDragStart={handleDragStart} />
     </a>,
   ];
 
