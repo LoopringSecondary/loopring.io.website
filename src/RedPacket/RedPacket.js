68c65978ffdbb8fa89287a8b65b3e641db26bbc7
wangdong <wangdong@zhongan.io>
2021-02-02 21:48:59 +0800

    more

--- src/RedPacket/RedPacket.js
+++ src/RedPacket/RedPacket.js
@@ -8,6 +8,7 @@ import {Translate} from 'react-localize-redux';
 import englishTranslation from './i18n/RedPacket.en.json';
 import chineseTranslation from './i18n/RedPacket.zh.json';
 import I18nComponent from '../Components/I18nComponent';
+import {DownloadButton} from '../Wallet_en/HeroSection';
 
 import './RedPacket.scss';
 
@@ -17,14 +18,29 @@ class RedPacket extends I18nComponent {
   }
 
   render() {
+    const classes = this.props.isChinese ? 'card chinese' : 'card english';
+
     return (
       <div className="redpacket-page">
-        <section className="section has-text-centered is-vcentered is-centered is-large">
-          <FontAwesomeIcon icon={faSadTear} size="5x" />
-
-          <h1>hello</h1>
-          <div></div>
+        <section className="section has-text-centered is-vcentered is-centered">
+          <div className={classes} />
         </section>
+        <div className="downloadButtons has-text-centered is-vcentered is-centered">
+          <DownloadButton
+            small
+            url="https://play.google.com/store/apps/details?id=loopring.defi.wallet"
+            imageUrl="/images/download_google_play.png"
+          />
+          <DownloadButton
+            small
+            url="http://52.82.125.33:8082/LoopringWallet.apk"
+            imageUrl="/images/download_apk.png"
+          />
+          <DownloadButton
+            small
+            imageUrl="/images/download_apple_appstore.svg"
+          />
+        </div>
       </div>
     );
   }
